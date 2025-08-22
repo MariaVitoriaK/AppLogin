f89bc557fcee6b3553b193adb002af2214bebf8c
MariaVitoriaK <197960@upf.br>
2025-08-21 21:34:20 -0300

    Feito a MAIN

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:login/pages/homePage.dart';
+import 'package:login/pages/loginPage.dart';
+import 'package:login/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,5 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  return MaterialApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App mobile III',
+      theme: ThemeData(colorSchemeSeed: Colors.cyan[200]),
+      initialRoute: AppRoutes.login,
+      routes: {
+        AppRoutes.login: (_) => Loginpage(),
+        AppRoutes.home: (_) => HomePage(),
+      },
+      onUnknownRoute: (settings) =>
+          MaterialPageRoute(builder: (_) => Loginpage()),
+    );
+  }
 }
